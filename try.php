daaf5ed613630fefbc9056a2b2b48981a51003e6
Tumbo Lobi John <johnlobiit@gmail.com>
2022-09-01 11:13:46 +0300

    associative_array

---
+++ try.php
@@ -0,0 +1,27 @@
+
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<?php
+$name = 'loto'; //str
+$age = 40;
+$has_kids = false; //bool
+
+echo $name;
+
+// simple Array
+$numbers = [1,23,45,6,90];
+$fruits = array('apple', 'mango', 'orange', 'lemon');
+
+print_r($fruits);
+echo $numbers[3];
+
+$associative_array = [
+        'first_name' => 'Jon',
+        'last_name' => 'Lobi',
+        'emal'
+]
+?>
+</body>
